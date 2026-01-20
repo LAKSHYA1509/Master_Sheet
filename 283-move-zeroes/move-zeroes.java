34e32abc979dea5a770baf347020bff1601a0dba
LAKSHYA BHARDWAJ <lakshyabhardwaj200315@gmail.com>
2026-01-20 16:13:46 +0530

    Time: 2 ms (89.56%) | Memory: 47.7 MB (80.39%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,5 +1,7 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // Approach 1 -> Get a new List and then work on that return rest;
+        /*
         int n = nums.length;
         int [] res = new int[n];
         Arrays.fill(res,0);
@@ -12,6 +14,18 @@ class Solution {
         }
         for(int i=0;i<n;i++) {
             nums[i] = res[i];
+        }
+        */
+
+        // Approach 2 --> We will use Two Pointers
+        int i = 0;
+        for(int j = 0; j < nums.length; j++) {
+            if(nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
         }
     }
 }
