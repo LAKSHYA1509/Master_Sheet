c8cdc2f1191183fbbb9cf454ab576ef8f7b18827
LAKSHYA BHARDWAJ <lakshyabhardwaj200315@gmail.com>
2026-01-22 15:16:58 +0530

    Time: 6 ms (90.08%) | Memory: 58.7 MB (44.00%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,12 +1,48 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> res = new ArrayList<>();
-        boolean[] seen = new boolean[n + 1];
+        // Its good to define a space beforehand so to not put pressure back on heap
+        List<Integer> res = new ArrayList<>(n);
 
-        for (int num : nums) {
-            if (seen[num]) res.add(num);
-            else seen[num] = true;
+        // Approach 1 --> HashMap
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for(int i=0; i<n; i++) {
+//            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//            if(map.get(nums[i]) > 1) res.add(nums[i]);
+//        }
+
+        // Approach 2 --> HashSet
+
+//        Set<Integer> set = new HashSet<>();
+//        for (int i = 0; i < n; i++) {
+//            if (set.contains(nums[i])) res.add(nums[i]);
+//            set.add(nums[i]);
+//        }
+        // Approach 3 --> Bool array of seen'
+
+//        boolean[] seen = new boolean[n + 1];
+//        for (int num : nums) {
+//            if (seen[num]) res.add(num);
+//            else seen[num] = true;
+//        }
+
+        // Approach 4 --> Nested Loop (Worst of all)
+
+
+//        for (int i = 0; i < n; i++) {
+//            for (int j = i + 1; j < n; j++) {
+//                if (nums[i] == nums[j]) res.add(nums[i]);
+//            }
+//        }
+
+        // Approach 5 --> Cyclic Sort(Negate what is present in that position and you will find ahead)
+        // Imagine you have a switch which you can only turn once and not again and again, Cyclic sort helps in this case by marking index already present to negate in the sign
+        for(int i = 0; i < n; i++) {
+            int val = Math.abs(nums[i]);
+            int idx = val - 1;
+
+            if(nums[idx] < 0) res.add(val);
+            nums[idx] = -nums[idx];
         }
         return res;
     }
