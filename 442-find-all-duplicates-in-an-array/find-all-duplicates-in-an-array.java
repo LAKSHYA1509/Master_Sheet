c1acb74d9070b3a77510863c16207f3557a38632
LAKSHYA BHARDWAJ <lakshyabhardwaj200315@gmail.com>
2026-01-22 15:05:48 +0530

    Time: 4 ms (97.59%) | Memory: 60.4 MB (42.90%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,27 +1,13 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         int n = nums.length;
-        // Its good to define a space beforehand so to not put pressure back on heap
-        List<Integer> res = new ArrayList<>(n);
+        ArrayList<Integer> res = new ArrayList<>();
+        boolean[] seen = new boolean[n + 1];
 
-        // Approach 1 --> HashMap
-//        HashMap<Integer, Integer> map = new HashMap<>();
-//        for(int i=0; i<n; i++) {
-//            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-//            if(map.get(nums[i]) > 1) res.add(nums[i]);
-//        }
-
-        // Approach 2 --> HashSet
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            if (set.contains(nums[i])) res.add(nums[i]);
-            set.add(nums[i]);
+        for (int num : nums) {
+            if (seen[num]) res.add(num);
+            else seen[num] = true;
         }
-        // Approach 3 --> Bool array of seen
-
-        // Approach 4 --> Nested Loop (Worst of all)
-
-        // Approach 5 --> Cyclic Sort(Negate what is present in that position and you will find ahead)
         return res;
     }
 }
