26fa5a9ccb20678a3d2369b804c3e37c212a98ab
LAKSHYA BHARDWAJ <lakshyabhardwaj200315@gmail.com>
2026-01-28 14:24:00 +0530

    Time: 16 ms (21.89%) | Memory: 47 MB (7.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,17 +4,20 @@ class Solution {
         int m = t.length();
         if (n != m) return false;
 
-        // Sorting
-        char[] s1 = s.toCharArray();
-        char[] t1 = t.toCharArray();
-        Arrays.sort(s1);
-        Arrays.sort(t1);
-        for(int i = 0; i < m; i++) {
-            if(s1[i]!=t1[i]) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        // HashMap
-
+        for(int i = 0; i < m; i++) {
+            if(map.containsKey(t.charAt(i))) {
+                map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+                if(map.get(t.charAt(i)) == 0) map.remove(t.charAt(i));
+            }
+        }
         // Character frequency check
-        return true;
+//        char[] s1 = s.toCharArray();
+//        char[] t1 = t.toCharArray();
+        return map.isEmpty();
     }
 }
